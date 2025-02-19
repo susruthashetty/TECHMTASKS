a2d8acd37aa6d6cdd8823130f05e2d9a4993e786
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-02-19 14:44:27 +0530

    Update ItemBeanRunner.java

--- Daily_tasks/17-02-2025/Task3/ItemBeanRunner.java
+++ Daily_tasks/17-02-2025/Task3/ItemBeanRunner.java
@@ -1,1 +1,19 @@
+package examples;
 
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.xml.*;
+import org.springframework.core.io.*;
+public class ItemBeanRunner {
+	public static void main(String[] args)
+	{
+		// inclusion of XML File
+		Resource res = new ClassPathResource("config.xml");
+		@SuppressWarnings("depreacation")
+		BeanFactory factory = new XmlBeanFactory(res);
+		
+		// inject the values via XML File 
+		
+		ItemsBean ibn = (ItemsBean)factory.getBean("itemBean");
+		ibn.displayDetails();
+	}
+}
