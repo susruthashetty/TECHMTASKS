1b0af63c667c699431f6b1ef8f11aa3ff4357dfd
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-02-28 23:12:40 +0530

    Create Task6.java

---
+++ Daily_tasks/14-2-2025/JUnit Main/Task6.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Task6 {
+	public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+}
