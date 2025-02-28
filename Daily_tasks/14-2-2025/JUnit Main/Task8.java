4ddfc81ee6839100c9ba82778c4aff4e0cc97f76
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-02-28 23:14:05 +0530

    Create Task8.java

---
+++ Daily_tasks/14-2-2025/JUnit Main/Task8.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Task8 {
+	private int cube(int num) {
+        return num * num * num;
+    }
+
+    public int getCube(int num) {
+        return cube(num);
+    }
+
+}
