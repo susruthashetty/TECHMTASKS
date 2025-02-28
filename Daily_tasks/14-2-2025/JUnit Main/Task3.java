84f377d0e5c58437168514f36927079b3c483852
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-02-28 23:10:35 +0530

    Create Task3.java

---
+++ Daily_tasks/14-2-2025/JUnit Main/Task3.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class JUnit_Task3 {
+	public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+}
