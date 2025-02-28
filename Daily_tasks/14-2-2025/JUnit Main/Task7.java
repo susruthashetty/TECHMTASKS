fa65cb2193ada2c984fc507c05e462c68e72e0fd
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-02-28 23:13:11 +0530

    Create Task7.java

---
+++ Daily_tasks/14-2-2025/JUnit Main/Task7.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class JUnit_Task7 {
+	public int add(int a, int b) {
+        return a + b;
+    }
+
+    public boolean isPositive(int num) {
+        return num > 0;
+    }
+
+}
