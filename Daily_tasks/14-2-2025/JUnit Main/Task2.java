ed4b72cee34480900ca7feab25d7b6a4227f8bbd
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-02-28 23:10:04 +0530

    Update Task2.java

--- Daily_tasks/14-2-2025/JUnit Main/Task2.java
+++ Daily_tasks/14-2-2025/JUnit Main/Task2.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class JUnit_Task2 {
+public class Task2 {
 	public void validateNumber(int num) {
         if (num < 0) {
             throw new IllegalArgumentException("Number must be non-negative");
