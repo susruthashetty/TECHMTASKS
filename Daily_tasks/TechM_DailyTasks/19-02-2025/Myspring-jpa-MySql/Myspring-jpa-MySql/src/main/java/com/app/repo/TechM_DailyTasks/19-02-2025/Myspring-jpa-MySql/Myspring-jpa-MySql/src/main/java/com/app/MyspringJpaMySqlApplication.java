b8d4231e526901516c1458c1d993d4a44c56ee49
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 15:03:51 +0530

    Create MyspringJpaMySqlApplication.java

---
+++ Daily_tasks/TechM_DailyTasks/19-02-2025/Myspring-jpa-MySql/Myspring-jpa-MySql/src/main/java/com/app/repo/TechM_DailyTasks/19-02-2025/Myspring-jpa-MySql/Myspring-jpa-MySql/src/main/java/com/app/MyspringJpaMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringJpaMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringJpaMySqlApplication.class, args);
+	}
+
+}
