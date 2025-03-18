e98e5485422bf2033d398eb7906752bb2f939322
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 13:55:37 +0530

    Create Task01.ts

---
+++ Daily_tasks/26-02-2025/TypeScript_Tasks/Task01.ts
@@ -0,0 +1,6 @@
+// 1. Write a TypeScript program that declares a variable `name` and assigns it a string value. Also declare a variable `age` and assign it a number value. Finally print the values of name and age.
+
+let name1: string = "Nithish";
+let age: number = 21;
+
+console.log(`Hello, my name is ${name1} and I am ${age} years old.`);
