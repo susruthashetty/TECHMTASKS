1d70636f27f08442c28d0178b47329e379d11ebf
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 13:57:30 +0530

    Create Task3.js

---
+++ Daily_tasks/26-02-2025/TypeScript_Tasks/Task3.js
@@ -0,0 +1,14 @@
+//3. Write a TypeScript program that declares variables of the following data types: number, string, boolean, and undefined. Assign values to them and perform basic operations.
+var n1 = 30;
+var str = "Hello";
+var boolVariable = true;
+var undefinedVariable = undefined;
+console.log("Sum:", n1 + 10);
+console.log("Concatenated String:", str + " world!");
+console.log("Logical AND:", boolVariable && true);
+if (undefinedVariable === undefined) {
+    console.log("undefinedVariable is undefined.");
+}
+else {
+    console.log("undefinedVariable is defined.");
+}
