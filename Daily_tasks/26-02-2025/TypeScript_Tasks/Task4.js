93ea484ca5bbb1e284f5203436850bed6a0cb95c
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 13:58:54 +0530

    Create Task4.js

---
+++ Daily_tasks/26-02-2025/TypeScript_Tasks/Task4.js
@@ -0,0 +1,4 @@
+//4. Write a TypeScript program that declares a variable as a number and then tries to assign a string to it to see type checking in action.
+var n = 42;
+//n = "Hello, TypeScript!"; // This line will result in a type error
+console.log("n = ", n);
