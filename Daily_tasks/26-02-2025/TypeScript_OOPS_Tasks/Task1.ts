2d6fde5688d023dba5b1425a30b5aaabaee7a24d
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 14:09:08 +0530

    Create Task1.ts

---
+++ Daily_tasks/26-02-2025/TypeScript_OOPS_Tasks/Task1.ts
@@ -0,0 +1,16 @@
+//1. Write a TypeScript class called Bus with the properties make, model, and year. Implement a constructor that initializes these properties when a Bus object is created.
+
+class Bus {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+}
+
+const myBus = new Bus("Mercedes", "Sprinter", 2022);
+console.log(myBus);
