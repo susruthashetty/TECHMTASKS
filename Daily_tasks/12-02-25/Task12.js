caf04a8b4f32d92c37bec70aeaddcf8b5b8f9cff
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 14:27:27 +0530

    Create Task12.js

---
+++ Daily_tasks/12-02-25/Task12.js
@@ -0,0 +1,10 @@
+let number_1 = 713;
+let number_2 = 200;
+
+if (number_1 > number_2) {
+  console.log(number_1 + " is the largest number.");
+} else if (number_2 > number_1) {
+  console.log(number_2 + " is the largest number.");
+} else {
+  console.log("Both are equal.");
+}
