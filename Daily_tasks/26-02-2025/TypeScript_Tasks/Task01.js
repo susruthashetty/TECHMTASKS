383bd53bb01e731d6936a7702ce82abd99cb0839
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 13:54:55 +0530

    Create Task01.js

---
+++ Daily_tasks/26-02-2025/TypeScript_Tasks/Task01.js
@@ -0,0 +1,4 @@
+// 1. Write a TypeScript program that declares a variable `name` and assigns it a string value. Also declare a variable `age` and assign it a number value. Finally print the values of name and age.
+var name1 = "Nitish";
+var age = 21;
+console.log("Hello, my name is ".concat(name1, " and I am ").concat(age, " years old."));
