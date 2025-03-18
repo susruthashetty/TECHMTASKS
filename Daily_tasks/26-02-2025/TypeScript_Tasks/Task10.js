b3cad98852379253867282bfc23afdba2e8f9ea7
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 14:06:06 +0530

    Create Task10.js

---
+++ Daily_tasks/26-02-2025/TypeScript_Tasks/Task10.js
@@ -0,0 +1,5 @@
+//10. Write a TypeScript program that declares a variable 'isNull' and assigns it null. You should also declare a variable called 'isUndefined' and assign it an undefined value.
+var isNull = null;
+var isUndefined = undefined;
+console.log("isNull:", isNull);
+console.log("isUndefined:", isUndefined);
