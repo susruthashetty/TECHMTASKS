0858950148c6e06de6a3b04b2ac2c46d2943cb0a
susruthashetty <113044835+susruthashetty@users.noreply.github.com>
2025-03-18 13:31:03 +0530

    Create Task12.java

---
+++ Daily_tasks/Day5_tasks/Collections/Task12.java
@@ -0,0 +1,28 @@
+package day5_tasks.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Iterator;
+
+public class Task12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> numbers = new LinkedList<>();
+		Scanner scan = new Scanner(System.in);
+		numbers.add(1);
+		numbers.add(4);
+		numbers.add(2);
+		numbers.add(6);
+		numbers.add(3);
+		numbers.add(6);
+		
+		
+		System.out.println("Numbers are : ");
+		Iterator<Integer> it2 = numbers.iterator();
+		while(it2.hasNext()) {
+			System.out.printf("%d ", it2.next());
+		}
+		scan.close();
+	}
+
+}
